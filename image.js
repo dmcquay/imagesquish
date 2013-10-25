ec047f221642944c9da81dfbbe07ac40f69a58da
Dustin McQuay <dmcquay@gmail.com>
2013-10-24 23:22:59 -0600

    limit the number of concurrent image manipulations

--- image.js
+++ image.js
@@ -1,28 +1,32 @@
 var config = require('./config');
 var gm = require('gm');
 var keyUtil = require('./key-util');
+var semaphore = require('semaphore');
 var storage = require('./storage');
 
+var sem = semaphore(config.maxConcurrentManipulations || 2);
+
 exports.doManipulation = function(bucket, imgId, manipulation, cb) {
-    var srcKey = keyUtil.generateKey(bucket, imgId);
-    var destKey = keyUtil.generateKey(bucket, imgId, manipulation);
-    storage.getObject(srcKey, function(err, res) {
-        var img = gm(res.Body);
+    sem.take(function(done) {
+        var srcKey = keyUtil.generateKey(bucket, imgId);
+        var destKey = keyUtil.generateKey(bucket, imgId, manipulation);
+        storage.getObject(srcKey, function(err, res) {
+            var img = gm(res.Body);
+            var steps = config.buckets[bucket].manipulations[manipulation];
+            for (var i = 0; i < steps.length; i++) {
+                step = steps[i];
+                img[step.operation].apply(img, step.params);
+            }
 
-        var steps = config.buckets[bucket].manipulations[manipulation];
-
-        for (var i = 0; i < steps.length; i++) {
-            step = steps[i];
-            img[step.operation].apply(img, step.params);
-        }
-
-        img.toBuffer(function(err, buffer) {
-            storage.upload({
-                data: buffer,
-                key: destKey,
-                contentType: res.ContentType
-            }, function(err, s3res) {
-                cb(err);
+            img.toBuffer(function(err, buffer) {
+                storage.upload({
+                    data: buffer,
+                    key: destKey,
+                    contentType: res.ContentType
+                }, function(err, s3res) {
+                    cb(err);
+                    sem.leave();
+                });
             });
         });
     });
