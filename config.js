525802af68a0924df67bc63e363efe62ed394e34
Dustin McQuay <dmcquay@gmail.com>
2013-12-10 00:42:54 -0700

    Bucket config inheritance.

--- config.js
+++ config.js
@@ -1,2 +1,21 @@
 var konphyg = require('konphyg')(__dirname + '/config');
-module.exports = konphyg('config');
+var config = konphyg('config');
+
+var bucketName;
+
+for (bucketName in config.buckets) {
+    var bucket = config.buckets[bucketName];
+    var key, inherit;
+    if (bucket.inheritFrom) {
+        inherit = config.buckets[bucket.inheritFrom];
+        if (!inherit) {
+            throw Error('Bucket ' + bucketName + ' tried to inherit from '
+                + bucket.inheritFrom + ' which does not exist.');
+        }
+        for (key in inherit) {
+            bucket[key] = bucket[key] || inherit[key];
+        }
+    }
+}
+
+module.exports = config;
