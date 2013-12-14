e9b64f1539df360ee15452680fbb9e4bcaeedd63
Dustin McQuay <dmcquay@gmail.com>
2013-12-14 14:40:33 -0700

    Add crop operation.

--- operations.js
+++ operations.js
@@ -1,7 +1,30 @@
 module.exports = {
+    /**
+     * Deprecated. Use crop instead.
+     * This function is the equivalent of crop(size, size).
+     *
+     * @param size
+     */
     squareCenterCrop: function(size) {
         return this.resize(size, size, "^").
             gravity("Center").
             extent(size, size);
+    },
+
+    /**
+     * Crops an image. The resulting image will always be exactly
+     * [width] wide and [height] tall. By default it will center the
+     * crop area inside the original image, but you can control this
+     * with the third parameter (gravity).
+     *
+     * @param width
+     * @param height
+     * @param gravity
+     */
+    crop: function(width, height, gravity) {
+        gravity = gravity || "Center";
+        return this.resize(width, height, "^").
+            gravity(gravity).
+            extent(width, height);
     }
 };
