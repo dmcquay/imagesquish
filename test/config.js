ccd6bd5c2f267f4f8d6ca45fe6bbf3bc9d42945d
Dustin McQuay <dmcquay@gmail.com>
2015-08-30 16:01:51 -0600

    add unit test for bucket inheritance

---
+++ test/config.js
@@ -0,0 +1,34 @@
+var assert = require('assert');
+var config = require('../config');
+
+describe('config', function() {
+    it('buckets can inherit', function() {
+        var buckets = {
+            "main": {
+                "originHost": "example.com",
+                "manipulationsS3Bucket": "com-example-manipulations",
+                "allowOTFManipulations": true,
+                "manipulations": {
+                    "brand": [
+                        { "operation": "quality", "params": [90] }
+                    ],
+                    "landing_banner": [
+                        { "operation": "resize", "params": [800, null] },
+                        { "operation": "crop", "params": [800, 320] },
+                        { "operation": "quality", "params": [90] }
+                    ]
+                }
+            },
+            "dev": {
+                "inheritFrom": "main",
+                "manipulationsS3Bucket": "com-example-manipulations-dev",
+                "allowOTFManipulations": false
+            }
+        };
+        config._populateInheritedBuckets(buckets);
+        assert.equal(buckets.dev.allowOTFManipulations, false);
+        assert.equal(buckets.dev.originHost, 'example.com');
+        assert.deepEqual(buckets.dev.manipulations.brand, buckets.main.manipulations.brand);
+        assert.equal(buckets.dev.manipulations.brand[0].operation, 'quality');
+    });
+});
